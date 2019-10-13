a1b9a06ecb345cf653cd974d40c3bd405fda467b
datagutten <datagutten@datagutten.net>
2019-10-13 20:13:41 +0200

    New attempt to handle inclusion when running as vendor binary

--- utils/save_channels.php
+++ utils/save_channels.php
@@ -3,10 +3,10 @@
 
 use datagutten\dreambox\web\epg;
 
-if(file_exists(__DIR__.'/../vendor/autoload.php'))
-    require __DIR__.'/../vendor/autoload.php';
+if(file_exists(__DIR__.'/../autoload.php'))
+    require __DIR__.'/../autoload.php';
 else
-    require '../autoload.php';
+    require __DIR__.'/../vendor/autoload.php';
 
 try {
     $epg = new epg($argv[1]);
