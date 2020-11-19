76694d95a03dce65d513029c23ec7cd3e142f4c6
datagutten <datagutten@datagutten.net>
2020-11-19 20:47:46 +0100

    Update tests with new constructor argument Add dreambox IP as environment variable

--- tests/timerTest.php
+++ tests/timerTest.php
@@ -2,33 +2,47 @@
 
 
 use datagutten\dreambox\web\timer;
+use datagutten\tools\files\files;
 use PHPUnit\Framework\TestCase;
 
 class timerTest extends TestCase
 {
+    /**
+     * @var string
+     */
+    private $channel_file;
+    /**
+     * @var array|false|string
+     */
+    private $dreambox_ip;
+
     function setUp(): void
     {
-        $file = sprintf('%s/channels_127.0.0.1.json',  realpath(__DIR__.'/../src'));
         //$string = '"1:0:19:12D:9:46:FFFF016A:0:0:0:":"NRK Super\/NRK3","1:0:19:515:9:46:FFFF016A:0:0:0:":"NRK Super\/NRK3 Lydteks","1:0:1:5EC:1:46:FFFF014A:0:0:0:":"NRK Tegnspr\u00e5k"';
-        copy(__DIR__.'/test_channels_127.0.0.1.json', $file);
+        $ip_env = getenv('DREAMBOX_IP');
+        if(!empty($ip_env))
+            $this->dreambox_ip = $ip_env;
+        else
+            $this->dreambox_ip = '127.0.0.1';
+        $this->channel_file = files::path_join(__DIR__, 'test_channels_127.0.0.1.json');
     }
 
     public function test__construct()
     {
-        $timer = new timer('127.0.0.1');
+        $timer = new timer($this->dreambox_ip, $this->channel_file);
         $this->assertIsArray($timer->channels);
         $this->assertSame('NRK Super/NRK3', $timer->channels['1:0:19:12D:9:46:FFFF016A:0:0:0:']);
     }
     public function testGetChannelId()
     {
-        $timer = new timer('127.0.0.1');
+        $timer = new timer($this->dreambox_ip, $this->channel_file);
         $channel_id = $timer->channel_id('NRK Super/NRK3');
         $this->assertSame('1:0:19:12D:9:46:FFFF016A:0:0:0:', $channel_id);
     }
 
     public function testAdd_timer()
     {
-        $timer = new timer('127.0.0.1');
+        $timer = new timer($this->dreambox_ip, $this->channel_file);
         $start = strtotime('16:00');
         $end = strtotime('17:00');
         $channel_id = $timer->channel_id('NRK Super/NRK3');
