7d54cef195948f9d7b79a66c2fe0facfa6bbe8e7
datagutten <datagutten@datagutten.net>
2021-06-20 17:03:23 +0200

    Add tests for empty address argument and empty channel file

--- tests/commonTest.php
+++ tests/commonTest.php
@@ -8,6 +8,12 @@ use PHPUnit\Framework\TestCase;
 
 class commonTest extends TestCase
 {
+    public function testEmptyAddress()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new common('');
+    }
+
     public function testInvalidAddress()
     {
         $this->expectException(DreamboxHTTPException::class);
@@ -26,5 +32,16 @@ class commonTest extends TestCase
         $this->expectException(FileNotFoundException::class);
         $common = new common('127.0.0.1');
         $common->load_channel_list('bad');
+    }
+
+    public function testLoadChannelListEmpty()
+    {
+        $this->expectException(DreamboxException::class);
+        $this->expectExceptionMessage('Channel list empty');
+        $common = new common('127.0.0.1');
+        $fp = tmpfile();
+        fwrite($fp, json_encode([]));
+        $path = stream_get_meta_data($fp)['uri'];
+        $common->load_channel_list($path);
     }
 }
