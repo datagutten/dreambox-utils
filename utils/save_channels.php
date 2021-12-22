1ed6404519b458759fc933d43b5c9902ae5658e3
datagutten <datagutten@datagutten.net>
2021-12-22 15:22:57 +0100

    Add autoloader locator to script

--- utils/save_channels.php
+++ utils/save_channels.php
@@ -3,10 +3,7 @@
 
 use datagutten\dreambox\web\epg;
 
-if(file_exists(__DIR__.'/../../../autoload.php'))
-    require __DIR__.'/../../../autoload.php';
-else
-    require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/loader.php';
 
 try {
     $epg = new epg($argv[1]);
